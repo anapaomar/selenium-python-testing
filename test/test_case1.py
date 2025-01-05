b590eb5a88e832883c2cddf0dbb26d9f3c6fbbc5
Ana <anapaolam63@gmail.com>
2025-01-05 11:48:12 -0500

    Se añaden reportes con allure

--- test/test_case1.py
+++ test/test_case1.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager 
@@ -18,6 +19,8 @@ def setup():
     yield driver
     driver.quit()
 
+@allure.feature('Reserva de vuelo de ida')
+@allure.story('Realizar una reserva de vuelo de ida desde Colombia a España')
 def test_booking_one_way(setup):
     driver = setup
     homepage = HomePage(driver)
@@ -26,29 +29,37 @@ def test_booking_one_way(setup):
     servicespage = ServicePage(driver)
     seatmappage = SeatmapPage(driver)
 
-    homepage.set_language("English")
-    homepage.set_point_of_sale("Colombia")
-    homepage.set_journey("One way")
-    homepage.select_origin("CTG")
-    homepage.select_destination("MAD")
-    homepage.select_passengers(1,1,1,1)
-    homepage.searchFlight()
+    with allure.step("Establecer idioma y punto de venta"):
+        homepage.set_language("English")
+        homepage.set_point_of_sale("Colombia")
+    
+    with allure.step("Seleccionar detalles del viaje"):
+        homepage.set_journey("One way")
+        homepage.select_origin("CTG")
+        homepage.select_destination("MAD")
+        homepage.select_passengers(1,1,1,1)
+        homepage.searchFlight()
 
-    flightpage.select_journey_price()
-    flightpage.select_flight_type("light")
-    flightpage.select_continue()
+    with allure.step("Seleccionar precio y tipo de vuelo"):
+        flightpage.select_journey_price()
+        flightpage.select_flight_type("light")
+        flightpage.select_continue()
 
-    passengers_list = [
-        {'gender': 'M', 'name': 'Juan', 'lastname': 'Pérez'},
-        {'gender': 'F', 'name': 'Martha', 'lastname': 'Pérez'},
-        {'gender': 'F', 'name': 'Laura', 'lastname': 'Pérez'},
-        {'gender': 'M', 'name': 'Carlos', 'lastname': 'Pérez'}
-    ]
-    passengerspage.fillin_passenger_form(passengers_list)
-    passengerspage.fillin_reservation_holder("57","32545698","jperez@gmail.com")
-    passengerspage.select_continue()
+    with allure.step("Llenar formulario de pasajeros"):
+        passengers_list = [
+            {'gender': 'M', 'name': 'Juan', 'lastname': 'Pérez'},
+            {'gender': 'F', 'name': 'Martha', 'lastname': 'Pérez'},
+            {'gender': 'F', 'name': 'Laura', 'lastname': 'Pérez'},
+            {'gender': 'M', 'name': 'Carlos', 'lastname': 'Pérez'}
+        ]
+        passengerspage.fillin_passenger_form(passengers_list)
+    
+    with allure.step("Llenar datos del titular de la reserva"):
+        passengerspage.fillin_reservation_holder("57","32545698","jperez@gmail.com")
+        passengerspage.select_continue()
 
-    servicespage.select_continue()
+    with allure.step("No seleccionar servicios en la página de servicios"):
+        servicespage.select_continue()
 
     #seatmappage.select_seat("economy")
 
